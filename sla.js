54097ee0376f5ca28bd250e52e47a56811ac4782
Peter-0066 <salloumbeter@gmail.com>
2025-05-09 10:02:27 +0300

    Add files via upload

---
+++ sla.js
@@ -0,0 +1,37 @@
+var price = 1 ;
+
+
+function dgr(d){
+if (d==1){
+    price = 100;
+}
+else if (d==2){
+    price = 200;
+}
+else if (d==3){
+    price = 400;
+}
+}
+function numt(n){
+if(n==1){
+price = price ;
+}
+else if (n==2){
+    price = price*2;
+}
+else if (n==3){
+    price = price*3;
+}
+else if (n==4){
+    price = price*4;
+}
+else if (n==5){
+    price = price*5;
+}
+else if (n==6){
+    price = price*6;
+}
+}
+function nstop(){
+    price = price*1.5;
+}
